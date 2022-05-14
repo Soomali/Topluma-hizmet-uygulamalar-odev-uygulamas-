c6c0adf10bb728a894389aa02ec76b43ad301ff3
Soomali <mehmetaliguunaydin@gmail.com>
2022-05-14 14:50:26 +0300

    changed state callbacks

--- lib/state/ui_notifier.dart
+++ lib/state/ui_notifier.dart
@@ -6,35 +6,37 @@ class UINotifier extends ChangeNotifier {
 
   void update(UIEvent event) {
     if (event is AnimationStartedEvent) {
-      onAnimationStart();
+      _onAnimationStart(event);
     } else if (event is _AnimationEndedEvent) {
-      onAnimationEnd();
+      _onAnimationEnd();
     } else if (event is FoodSelectedEvent) {
-      onFoodSelected();
+      _onFoodSelected();
     } else if (event is FoodRefreshEvent) {
-      onFoodRefresh();
+      _onFoodRefresh();
     } else {
       throw 'Unknown event ${event.runtimeType} for UINotifier.';
     }
   }
 
-  void onFoodRefresh() {
+  void _onFoodRefresh() {
     _state = _state.copyWith(foods: createFoodPair());
     notifyListeners();
   }
 
-  void onAnimationStart() {
+  void _onAnimationStart(AnimationStartedEvent event) {
     _state = _state.copyWith(canSelectFood: false, isAnimating: true);
     notifyListeners();
+    Future.delayed(Duration(milliseconds: event.animationMillis))
+        .then((value) => update(_AnimationEndedEvent()));
   }
 
-  void onAnimationEnd() {
-    _state = _state.copyWith(
-        canSelectFood: true, foods: createFoodPair(), isAnimating: false);
+  void _onAnimationEnd() {
+    _state = _state.copyWith(canSelectFood: true, isAnimating: false);
     notifyListeners();
+    update(FoodRefreshEvent());
   }
 
-  void onFoodSelected() {
+  void _onFoodSelected() {
     _state = _state.copyWith(canSelectFood: false);
     notifyListeners();
   }
